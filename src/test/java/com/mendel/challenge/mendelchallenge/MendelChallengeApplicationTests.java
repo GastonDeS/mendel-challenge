a94cd399309945dc816f7664c05ec88438d04103
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2023-11-30 10:14:42 -0300

    Initial setup

---
+++ src/test/java/com/mendel/challenge/mendelchallenge/MendelChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mendel.challenge.mendelchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MendelChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
